94a3012cd9821bf7f18b9e538c44e5f7c6493bd5
Steven Bouwkamp <lordstevex@gmail.com>
2016-02-20 13:54:11 -0500

    Removed ./idea directory

--- java/Hand.java
+++ java/Hand.java
@@ -1,3 +1,33 @@
 
 public class Hand {
+
+    private String cards;
+
+    public Hand(String cards) {
+        this.cards  = cards;
+    }
+
+    public String getCards() {
+        return cards;
+    }
+
+    public void setCards(String cards) {
+        this.cards = cards;
+    }
+
+    public boolean isProperFormat(String cards) {
+
+        return false;
+    }
+
+    public String toString() {
+        String result = "";
+        for(int i = 0; i < cards.length() - 1; ++i) {
+            if(i % 2 == 1)
+                result += " ";
+            result += cards.substring(i, i+2);
+        }
+        return result;
+    }
+
 }
--- java/HandEvaluator.java
+++ java/HandEvaluator.java
@@ -2,4 +2,16 @@
 public class HandEvaluator {
 
 
+
+    public static void main(String args[]) {
+        String cards = "AcAsKdKcTh9h7d6s3d5c";
+
+        Hand hand1 = new Hand(cards.substring(0, 9));
+        Hand hand2 = new Hand(cards.substring(10));
+        System.out.println(hand1.toString());
+        System.out.println(hand2.toString());
+    }
+
+
+
 }
