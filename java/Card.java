2adb3ac6b37551a0d5fc74083c10fc3777ff6f81
Steven Bouwkamp <lordstevex@gmail.com>
2016-02-24 11:43:23 -0500

    Refactored code within Card.java

--- java/Card.java
+++ java/Card.java
@@ -4,33 +4,39 @@ public class Card {
 
     private char rank;
     private char suit;
-    private int value;
+    private int value; // overall integer representation of a Card, a higher value doesn't always mean better Card
 
-    public final static int CLUBS = 0;
-    public final static int DIAMONDS = 1;
-    public final static int HEARTS = 2;
-    public final static int SPADES = 3;
+    private final int CLUBS = 0;
+    private final int DIAMONDS = 1;
+    private final int HEARTS = 2;
+    private final int SPADES = 3;
 
-    public final static int BAD_CARD = -1;
-    public final static int TWO = 0;
-    public final static int THREE = 1;
-    public final static int FOUR = 2;
-    public final static int FIVE = 3;
-    public final static int SIX = 4;
-    public final static int SEVEN = 5;
-    public final static int EIGHT = 6;
-    public final static int NINE = 7;
-    public final static int TEN = 8;
-    public final static int JACK = 9;
-    public final static int QUEEN = 10;
-    public final static int KING = 11;
-    public final static int ACE = 12;
+    private final int BAD_CARD = -1;
+    private final int TWO = 0;
+    private final int THREE = 1;
+    private final int FOUR = 2;
+    private final int FIVE = 3;
+    private final int SIX = 4;
+    private final int SEVEN = 5;
+    private final int EIGHT = 6;
+    private final int NINE = 7;
+    private final int TEN = 8;
+    private final int JACK = 9;
+    private final int QUEEN = 10;
+    private final int KING = 11;
+    private final int ACE = 12;
 
-    public final static int NUM_RANKS = 13;
+    private final int NUM_RANKS = 13;
 
-    private int iRank;
-    private int iSuit;
 
+    private int iRank; // integer representation of the Card's rank
+    private int iSuit; // integer representation of the Card's suit
+
+    /**
+     * Constructor for the Card object that takes two char parameters for its rank and suit
+     * @param rank is the char that will be the rank of the card [AaKkQqJjTt2-9]
+     * @param suit is the char that will be the suit of the card [CcDdHhSs]
+     */
     public Card(char rank, char suit) {
         this.rank = rank;
         this.suit = suit;
@@ -39,6 +45,10 @@ public class Card {
         iSuit = calcSuit(value);
     }
 
+    /**
+     * Constructor for the Card object that takes an integer value and derives its rank and suit
+     * @param value is the integer value of the card to derive [0, 51]
+     */
     public Card(int value) {
         this.value = value;
         iRank = calcRank(this.value);
@@ -47,6 +57,17 @@ public class Card {
         suit = getSuitFromValue(this.value);
     }
 
+    /**
+     * Calculates the integer value of the card in its rank, suit, and overall value.
+     *
+     * Rank - integer rankings start at 0 going from deuce to ace
+     * Suit - integer values for suit start at 0 going from Clubs, Diamonds, Hearts, Spades
+     *
+     * Note - a higher value doesn't necessarily mean a better card.
+     * @param rank the Card's rank as a char to convert into its given integer value
+     * @param suit the Card's suit as a char to convert into its given integer value
+     * @return the integer representation of that Card
+     */
     private int toValue(char rank, char suit) {
         int r = -1;
         switch (rank) {
@@ -81,10 +102,15 @@ public class Card {
             case 'C': s = CLUBS; break;
         }
         if (s != -1 && r != -1)
-            return calcIndex(r,s);
+            return calcValue(r,s);
         else return BAD_CARD;
     }
 
+    /**
+     * Takes a given integer [0, 51] and determines what its playing card rank value is in char form.
+     * @param value is the integer representation that that Card's overall value
+     * @return a char representing the Card's rank
+     */
     private char getRankFromValue(int value) {
         int r = calcRank(value);
         switch (r) {
@@ -119,6 +145,11 @@ public class Card {
         }
     }
 
+    /**
+     * Takes a given integer value [0, 51] and determines what its playing card suit value is in char form.
+     * @param value is the integer representation that that Card's overall value
+     * @return a char representing the Card's suit
+     */
     private char getSuitFromValue(int value) {
         int s = calcSuit(value);
         switch (s) {
@@ -135,22 +166,46 @@ public class Card {
         }
     }
 
-    private int calcIndex(int rank, int suit) {
+    /**
+     * Calculates the Card's overall integer value
+     * @param rank is the Card's integer rank value
+     * @param suit is the Card's integer suit value
+     * @return an integer that represents the overall value of the Card
+     */
+    private int calcValue(int rank, int suit) {
         return(suit*NUM_RANKS) + rank;
     }
 
+    /**
+     * Calculates the Card's integer rank from its overall value
+     * @param value is the integer representation of the Card's overall value
+     * @return an integer that represents the Card's rank value
+     */
     public int calcRank(int value) {
         return value % NUM_RANKS;
     }
 
+    /**
+     * Calculates the Card's integer suit from its overall value
+     * @param value is the integer representation of the Card's overall value
+     * @return an integer that represents the Card's suit value
+     */
     public int calcSuit(int value) {
         return value / NUM_RANKS;
     }
 
+    /**
+     * Gets the integer representation of the Card's rank value
+     * @return an integer that represents the Card's rank
+     */
     public int getIRank() {
         return iRank;
     }
 
+    /**
+     * Gets the integer representation of the Card's suit value
+     * @return an integer that represents the Card's suit
+     */
     public int getISuit() {
         return iSuit;
     }
@@ -160,6 +215,12 @@ public class Card {
     }
 
     static class CardComparator implements Comparator<Card> {
+        /**
+         * Compares two Cards and determines which is the higher ranked Card
+         * @param c1 is the first Card to compare
+         * @param c2 is the second Card to compare
+         * @return an integer that represents whether the first Card is greater than, less than, or equal to the second
+         */
         @Override
         public int compare(Card c1, Card c2) {
             return Integer.compare(c1.getIRank(), c2.getIRank());
