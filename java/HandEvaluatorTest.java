58f97e4db36d9962b1a3eea8858df8a2f18b3434
Steven Bouwkamp <lordstevex@gmail.com>
2016-02-25 09:51:19 -0500

    Added a test case for the HandEvaluator

--- java/HandEvaluatorTest.java
+++ java/HandEvaluatorTest.java
@@ -6,6 +6,10 @@ public class HandEvaluatorTest {
 
     @Test
     public void testCompareHands() throws Exception {
+        HandEvaluator eval = new HandEvaluator("AcKcQcJcTcKhQhJhTh9h");
+        assertTrue(eval.compareHands() == 1);
 
+        HandEvaluator eval2 = new HandEvaluator("2d3s4h6d8dThTcTdTsAh");
+        assertTrue(eval2.compareHands() == 2);
     }
 }
